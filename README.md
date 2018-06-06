337730ffcac48a5046a9d236a6bb21b6f94c596d
Matt Rixman <matt.rixman@clover.com>
2018-06-06 12:32:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Which Tables Touched
+
+This is a python script which will accept a mysql general log file and print out the names of the tables that were mentioned.
+
+It does not parse the SQL.  Instead it connects to the database and finds the list of tables.  Then it finds which of those appear in the log file
+
+## To use
+
+#### Have python 3.3 or higher installed
+
+#### Create a virtual environment
+Do this just once (it will create a directory)
+
+`❯ ``python3 -m venv which-venv`
+
+#### Enter a virtual environment
+
+`❯ ``source which-venv/bin/activate`
+
+#### Prepare environment
+Do this just once
+
+`which-venv ❯ ``pip install --upgrade pip`
+`which-venv ❯ ``pip install -r requirements.txt`
+
+#### Run it
+
+`which-venv ❯ ``python which_tables.py mysql_general.log --database meta --user root --password test`
+
+#### Exit virtual environment
+
+`which-venv ❯ ``deactivate`
+`❯ `
+
+## For more, try --help
+
+```
+which-venv ❯ python which_tables.py --help
+
+    usage: which_tables.py [-h] [-d DATABASE] [-u USER] [-p PASSWORD] file
+
+    positional arguments:
+      file
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -d DATABASE, --database DATABASE
+                            the name of the database whose tables we want
+                            (default: moonpie)
+      -u USER, --user USER  the mysql username (default: root)
+      -p PASSWORD, --password PASSWORD
+                            the mysql password (default: thangs)
+```
---
+++ which_tables.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# given a database and a mysql general log file, list the tables that were touched during the log session
+
+import os
+import MySQLdb
+import argparse
+import re
+from sys import platform
+from collections import namedtuple
+from sh import netstat, awk, bash
+
+def parse_args():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # some defaults
+    ddatabase = 'moonpie'
+    duser = 'root'
+    dpasswd= 'thangs'
+
+    # not always necessary, this works around an oddity I found one day
+    #if platform == "linux" or platform == "linux2":
+    #    dsocket= str(awk(netstat('-ln'), '/mysql(.*)?\.sock/ { print $9 }')).strip()
+    #elif platform == "darwin":
+    #    dsocket= str(awk(netstat('-an'), '/mysql(.*)?\.sock/ { print $5 }')).strip()
+    #else:
+    #    print("please specify the mysql socket explicitly")
+    #    dsocket=""
+
+    parser.add_argument('-d', '--database', default=ddatabase, help='the name of the database whose tables we want')
+    parser.add_argument('-u', '--user', default=duser, help='the mysql username')
+    parser.add_argument('-p', '--password', default=dpasswd, help='the mysql password')
+    #parser.add_argument('-s', '--socket', default=dsocket, help='the Unix socket for the mysql daemon')
+    parser.add_argument('file', type=str)
+
+    return parser.parse_args()
+
+# preprocess the file to reduce its size
+# (I expect the utilities below are faster than I can manage with python,
+#  and easier than I can manage with anything else)
+def get_candidates(in_file):
+
+    with open(in_file, 'r') as log:
+
+        # this is slow
+        #      uniq(
+        #          sort(
+        #              egrep(
+        #                  tr(
+        #                      tr(
+        #                          cat(in_file),
+        #                          ['-s', '[:space:]', r'\n']),            # spaces -> newlines
+        #                      ['-c', '-d', '[a-zA-Z0-9][:space:][_\-]']), # strip non-table-name characters
+        #                  ['-v', '[0-9]'])                                # ignore id's
+        #              )                                                   # sort
+        #          )                                                       # ignore duplicates
+        #      ).split()                                                   # as a list of words
+
+        # this is faster
+        command = '''cat {} | tr -s [:space:] '\n' | tr -c -d '[a-zA-Z0-9][:space:][_\-]' | egrep -v '[0-9]' | sort | uniq'''.format(in_file)
+        return str(bash(['-c', command])).split() # as a list of words
+
+def main(args):
+
+    # connect to the database
+    db=MySQLdb.connect(
+            host='localhost',
+            user=args.user,
+            passwd=args.password,
+            database=args.database)
+            # unix_socket=args.socket)
+
+    c = db.cursor()
+    c.execute("show tables;")
+
+    # get the set of table names
+    tables = set()
+    row = c.fetchone()
+    while row is not None:
+        tables.add(row[0])
+        row = c.fetchone()
+
+    # get the set of words in the log file
+    log_words = set(get_candidates(args.file))
+
+    # print their intersection
+    for table in list(tables.intersection(log_words)):
+        print(table)
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
